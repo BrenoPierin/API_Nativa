d4b3b8151b054d2ae03cce2ac52bb83716dc8e1a
unknown <brenomax02@gmail.com>
2020-11-19 15:47:43 -0300

    Camera finalizada

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Contatos from './pages/contatos';
+import toSpeech from './pages/TextToSpeech';
+import Localizacao from './pages/localizacao';
+import Cam from './pages/camera';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Contato" component={Contatos} />
+        <Tab.Screen name="Falar" component={toSpeech} />
+        <Tab.Screen name="Localização" component={Localizacao} />
+        <Tab.Screen name="Camera" component={Cam} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
